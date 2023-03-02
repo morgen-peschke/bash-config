4519bb7118c2de7403b3372cdbfb0be72f4f0f5c
Morgen Peschke <morgen.peschke@gmail.com>
2023-03-02 13:14:54 -0800

    Clean up curl.json

--- bin/curl.json.bash
+++ bin/curl.json.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
+#set -x
 
-__curl.process-data-argument() {
+args.process-data-argument() {
     if [[ '-' = "$1" ]]; then
         printf '%s' '"<stdin>"'
     elif [[ $(cut -c1 <<<"$1") = '@' ]]; then
@@ -11,13 +12,13 @@ __curl.process-data-argument() {
     fi
 }
 
-__curl.extractBody() {
+args.extract.requestBody() {
     data=()
     while [[ -n "${1:-}" ]]; do
         case "$1" in
         --json | --data | -d | --data-ascii | --data-binary | --data-urlencode)
             shift
-            data+=("$(__curl.process-data-argument "$1")")
+            data+=("$(args.process-data-argument "$1")")
             shift
             ;;
         --data-raw | --form | -F | --form-string | --form-escape)
@@ -33,33 +34,70 @@ __curl.extractBody() {
     jq --null-input --compact-output '$ARGS.positional' --jsonargs "${data[@]}"
 }
 
-__curl() {
-    local expectJson includeKeys response responseLines status body keys lastHeaderLine rawHeaders headers bodyArg keysArg reqSummary requestId requestBody
-    requestId=$(uuidgen | tr A-Z a-z | tr -d '\n')
-    expectJson="$1"
-    shift
-    includeKeys="$1"
-    shift
-    requestBody=$(__curl.extractBody "$@")
-    response=$(curl --silent --show-error -H "X-Request-Id=${requestId}" -iw '\n%{json}\n' "$@" >&1 || true)
-    responseLines=$(jq --slurp --raw-input 'gsub("\r\n";"\n")|split("\n")' <<<"$response")
-    status=$(jq '.[0] | if . == "" then null else . end' <<<"$responseLines")
-    body=$(jq --compact-output --raw-output '.[-3] | if . == "" then null else . end' <<<"$responseLines")
-    keys=$(jq --compact-output '.[-2] | fromjson' <<<"$responseLines")
-    lastHeaderLine=$(jq --raw-output --compact-output '.num_headers | tonumber | . + 1' <<<"$keys")
-    rawHeaders=$(jq --argjson lastHeaderLine "$lastHeaderLine" '.[1:$lastHeaderLine]' <<<"$responseLines")
-    headers=$(jq 'map(split(":") | {key: .[0], value: .[1:]|join(":")|ltrimstr(" ")} ) | from_entries' <<<"$rawHeaders")
+curl.returningLines() {
+    local curlArgs=("$@")
+    response=$(curl "${curlArgs[@]}" || true)
+    jq --slurp --raw-input 'gsub("\r\n";"\n")|split("\n")' <<<"$response"
+}
 
-    reqSummary=$(jq --null-input \
+extract.line() {
+    local lineNumber="${1:-0}" allLines="${2:-'[]'}" raw="${3:-}"
+    local outputArg='--compact-output'
+    [[ "$raw" = 'raw' ]] && outputArg='--raw-output'
+    jq <<<"$allLines" \
+        --argjson line "$lineNumber" \
+        "$outputArg" \
+        '.[$line] | if . == "" then null else . end'
+}
+
+output.build.summary() {
+    local keys requestBody curlArgs
+    keys="$1"
+    shift
+    requestBody="$1"
+    shift
+    curlArgs=("$@")
+    jq --null-input \
         --argjson keys "$keys" \
         --argjson body "$requestBody" \
-        '{ method: $keys.method, url: $keys.url, body: $body}')
+        '{ method: $keys.method, url: $keys.url, body: $body, curl: "curl \($ARGS.positional | @sh)"}' \
+        --args -- "${curlArgs[@]}"
+}
 
-    if [[ "$expectJson" = 'true' ]]; then
-        bodyArg=(--argjson body "$body")
-    else
-        bodyArg=(--arg body "$body")
-    fi
+response.extract.headers() {
+    local responseLines="$1" keys="$2"
+    local lastHeaderLine rawHeaders headers
+    lastHeaderLine=$(jq --raw-output --compact-output '.num_headers | tonumber | . + 1' <<<"$keys")
+    rawHeaders=$(jq --compact-output --argjson lastHeaderLine "$lastHeaderLine" '.[1:$lastHeaderLine]' <<<"$responseLines")
+    headers=$(jq --compact-output 'map(split(":") | {key: .[0], value: .[1:]|join(":")|ltrimstr(" ")} ) | from_entries' <<<"$rawHeaders")
+    jq --compact-output --null-input \
+        --argjson raw "$rawHeaders" \
+        --argjson parsed "$headers" \
+        '{raw: $raw, parsed: $parsed}'
+}
+
+response.extract.body() {
+    local responseLines="$1"
+    local rawBody parsedBody
+    rawBody=$(extract.line -3 "$responseLines")
+    jq <<<"$rawBody" --compact-output \
+        '{raw: ., parsed: (try fromjson catch .)}'
+}
+
+__curl() {
+    local curlArgs includeKeys responseLines status body keys rawHeaders headers keysArg reqSummary requestId requestBody
+    requestId="morgen-$(uuidgen | tr A-Z a-z | tr -d '\n')"
+    includeKeys="$1"
+    shift
+    requestBody=$(args.extract.requestBody "$@")
+    curlArgs=(--silent --show-error -H "X-Request-Id: ${requestId}" -iw '\n%{json}\n' "$@")
+    responseLines=$(curl.returningLines "${curlArgs[@]}")
+    status=$(extract.line 0 "$responseLines")
+    keys=$(extract.line -2 "$responseLines" raw)
+    headers=$(response.extract.headers "$responseLines" "$keys")
+    body=$(response.extract.body "$responseLines")
+    reqSummary=$(output.build.summary "$keys" "$requestBody" "${curlArgs[@]}")
+
     if [[ "$includeKeys" = 'true' ]]; then
         keysArg="$keys"
     else
@@ -67,20 +105,20 @@ __curl() {
     fi
 
     result=$(jq --null-input \
-        "${bodyArg[@]}" \
+        --argjson body "$body" \
         --argjson keys "$keysArg" \
         --argjson headers "$headers" \
         --argjson status "$status" \
         --argjson request "$reqSummary" \
         --arg requestId "$requestId" \
-        '{request: $request, requestId: $requestId, status: $status, keys: $keys, headers: $headers, body: $body}' 2>/dev/null)
-    if [[ $? -eq 0 ]]; then
+        '{request: $request, requestId: $requestId, status: $status, keys: $keys, headers: $headers.parsed, body: $body.parsed}' 2>/dev/null || true)
+    if [[ $result ]]; then
         printf '%s\n' "$result"
     else
         jq >&2 --null-input \
-            --arg body "$body" \
+            --argjson body "$body" \
             --argjson keys "$keysArg" \
-            --argjson headers "$rawHeaders" \
+            --argjson headers "$headers" \
             --argjson status "$status" \
             --argjson request "$reqSummary" \
             --arg requestId "$requestId" \
